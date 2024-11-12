eb4469037e17bf92c9163fa30247e4eeb43f4ffc
Georgina Kimani <njokikimani001@gmail.com>
2024-11-13 01:08:52 +0300

    corrected color to the sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import AppTheme from './shared-theme/AppTheme';
+import Dashboard from './pages/Dashboard';
+import AppNavbar from './components/AppNavbar';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   return (
     <div className="App">
-      <AppTheme />
-      <h1>App</h1>
+      <AppTheme>
+        <CssBaseline enableColorScheme/>
+          <AppNavbar />
+          <Dashboard  />
+      </AppTheme>
     </div>
   );
 }
--- frontend/src/components/SideMenu.js
+++ frontend/src/components/SideMenu.js
@@ -1,78 +1,37 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Avatar from '@mui/material/Avatar';
-import MuiDrawer, { drawerClasses } from '@mui/material/Drawer';
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import SelectContent from './SelectContent';
-import MenuContent from './MenuContent';
-import CardAlert from './CardAlert';
-import OptionsMenu from './OptionsMenu';
-
-const drawerWidth = 240;
-
-const Drawer = styled(MuiDrawer)({
-  width: drawerWidth,
-  flexShrink: 0,
-  boxSizing: 'border-box',
-  mt: 10,
-  [`& .${drawerClasses.paper}`]: {
-    width: drawerWidth,
-    boxSizing: 'border-box',
-  },
-});
+import React from 'react'
+import { slide as Menu } from "react-burger-menu";
+import './SideMenu.css';
+import AppTheme from '../shared-theme/AppTheme';
 
 export default function SideMenu() {
   return (
-    <Drawer
-      variant="permanent"
-      sx={{
-        display: { xs: 'none', md: 'block' },
-        [`& .${drawerClasses.paper}`]: {
-          backgroundColor: 'background.paper',
-        },
-      }}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          mt: 'calc(var(--template-frame-height, 0px) + 4px)',
-          p: 1.5,
-        }}
-      >
-        <SelectContent />
-      </Box>
-      <Divider />
-      <MenuContent />
-      <CardAlert />
-      <Stack
-        direction="row"
-        sx={{
-          p: 2,
-          gap: 1,
-          alignItems: 'center',
-          borderTop: '1px solid',
-          borderColor: 'divider',
-        }}
-      >
-        <Avatar
-          sizes="small"
-          alt="Riley Carter"
-          src="/static/images/avatar/7.jpg"
-          sx={{ width: 36, height: 36 }}
-        />
-        <Box sx={{ mr: 'auto' }}>
-          <Typography variant="body2" sx={{ fontWeight: 500, lineHeight: '16px' }}>
-            Riley Carter
-          </Typography>
-          <Typography variant="caption" sx={{ color: 'text.secondary' }}>
-            riley@email.com
-          </Typography>
-        </Box>
-        <OptionsMenu />
-      </Stack>
-    </Drawer>
-  );
+    <AppTheme>
+        <Menu>
+            <span className='SideMenuLogo'>
+                WAKA  ACADEMY
+                <br />
+                JUJA
+            </span>
+            <a className="menu-item" href="/Dashboard">
+            Dashboard
+            </a>
+            <a className="menu-item" href="/Students">
+            Students
+            </a>
+            <a className="menu-item" href="/Fees">
+            Fees
+            </a>
+            <a className="menu-item" href="/Staff">
+            Staff
+            </a>
+            <a className="menu-item" href="/Staff">
+            Staff
+            </a>
+            <a className="menu-item" href="/Sign Out">
+            Sign Out
+            </a>
+        </Menu>
+    </AppTheme>
+  )
 }
+
